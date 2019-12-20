a815116c527398af18f68ed1fd2a4c9fe1998381
Oaklee Ohmie <oakleeohmie@outlook.com>
2019-12-19 19:44:49 -0700

    added variables

--- NYTCode.js
+++ NYTCode.js
@@ -1,1 +1,12 @@
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=UeINUwtwl3qcPvAUR4rfzLuqRpnqSafX"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    $("#run-search").click(function () {
+        var searchTerm = $("#search-term").val().trim();
+        var startYear = $("#start-year").val().trim();
+        var endYear = $("#end-year").val().trim();
+    });
+});
