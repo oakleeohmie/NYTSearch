20cf388bbf43d85e41a60be4fdd338b6fa4082d0
Oaklee Ohmie <oakleeohmie@outlook.com>
2019-12-19 19:54:57 -0700

    got inputs to console.log

--- NYTCode.js
+++ NYTCode.js
@@ -1,12 +1,12 @@
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=UeINUwtwl3qcPvAUR4rfzLuqRpnqSafX"
+$('#run-search').on('click', function () {
+    event.preventDefault()
+    var searchTerm = $("#search-term").val().trim();
+    var startYear = $("#start-year").val().trim();
+    var endYear = $("#end-year").val().trim();
+    console.log(searchTerm);
+    console.log(startYear);
+    console.log(endYear);
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    $("#run-search").click(function () {
-        var searchTerm = $("#search-term").val().trim();
-        var startYear = $("#start-year").val().trim();
-        var endYear = $("#end-year").val().trim();
-    });
 });
+
