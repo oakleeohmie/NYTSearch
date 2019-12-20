67d4a0b0c94ed61c354ac7d09ae20f1d0134b968
Oaklee Ohmie <oakleeohmie@outlook.com>
2019-12-19 20:44:27 -0700

    added inputs to url

--- NYTCode.js
+++ NYTCode.js
@@ -1,12 +1,23 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=UeINUwtwl3qcPvAUR4rfzLuqRpnqSafX"
-$('#run-search').on('click', function () {
-    event.preventDefault()
+function buildQueryURL() {
     var searchTerm = $("#search-term").val().trim();
     var startYear = $("#start-year").val().trim();
     var endYear = $("#end-year").val().trim();
-    console.log(searchTerm);
-    console.log(startYear);
-    console.log(endYear);
+    var nytimes = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+    var url = nytimes + "q=" + searchTerm + "&end_date=" + endYear + "&begin_date=" + startYear + "&api-key=UeINUwtwl3qcPvAUR4rfzLuqRpnqSafX";
+    return (url);
+};
 
+function clear() {
+    $("#article-section").empty();
+};
+
+
+
+
+$('#run-search').on('click', function () {
+    event.preventDefault();
+    clear();
+    var url = buildQueryURL();
+    console.log(url);
 });
 
